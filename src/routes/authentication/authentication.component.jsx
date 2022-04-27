4a9714fbd0703dff39395280fd49d74397300117
JeffreyHoDev <JeffreyHoDev@gmail.com>
2022-04-27 21:00:32 +0800

    add authentication component and sign-in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,10 +1,11 @@
 // import { useEffect } from 'react'
 // import { getRedirectResult } from 'firebase/auth'
 
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
 
-const SignIn = () => {
+import './authentication.styles.scss'
+const Authentication = () => {
 
     // useEffect(async() => {
     //     const response = await getRedirectResult(auth)
@@ -14,10 +15,7 @@ const SignIn = () => {
     //     }
     // }, [])
 
-    const logGoogleUser = async() => {
-        const { user } = await signInWithGooglePopup()
-        const userDocRef = await createUserDocumentFromAuth(user)
-    }
+
     // const logGoogleRedirectUser = async() => {
     //     const { user } = await signInWithGoogleRedirect()
     //     console.log(user)
@@ -25,13 +23,12 @@ const SignIn = () => {
     // }
 
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google</button>
+        <div className='authentication-container'>
+            <SignInForm />          
             <SignUpForm />
             {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
         </div>
     )
 }
 
-export default SignIn
+export default Authentication
